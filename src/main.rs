f771e0e9c4fe853428b76c3abf5489e80871baa0
George Nicoll <george.nicoll@gmail.com>
2023-01-05 22:19:00 +0000

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod chapter1;
+mod chapter2;
 
 fn main() {
-    //chapter1::_1_3a();
-    chapter1::_1_3b();
+    chapter2::_2_3();
 }
 
 pub fn stop_dead_code_warnings() {
     chapter1::_1_3a();
+    chapter1::_1_3b();
+    chapter2::_2_3();
 }
